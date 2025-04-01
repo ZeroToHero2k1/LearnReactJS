0fa7e2266e378f3c2b15b1de44c1bc2e1b822c2d
ZeroToHero2k1 <toquyphuoc12a9@gmail.com>
2025-04-01 14:58:40 +0700

    mới học xong bài 14.2

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,11 @@
 
 import './App.css'
 import avatarPic from '../Image/avatar-cua-phuoc.jpg'
+import logo1 from '../Image/logo-1.jpg'
+import logo2 from '../Image/logo-2.png'
+import logo3 from '../Image/logo-3.webp'//lần sau nên bỏ hình trong folder assets
+import logo4 from '../Image/logo-4.webp'
+import myData from './data.js'
 
 let today=new Date().toLocaleDateString();
 let timenow=new Date().toLocaleTimeString()
@@ -35,15 +40,64 @@ function App() {
         <p>Họ và tên:{user.name}, tuổi:{user.age}, giới tính:{user.gender}</p>
         <p>Tình trạng: {hasGirlFriend?"đã có bạn gái":"chưa có bạn gái"}</p>
         <img src={avatarPic} alt="ảnh đại diện" />
+        <ul class="ngonNguPhuocDung">
+            <BaiTapProps image={logo1}
+            title="Đây là ngôn ngữ C#"
+            description="lorem idfcfsfgssfsfvsdfvsdgỵukjhgfgyjuikujhtgrfvsdvfcdvcvc"/>
+            <BaiTapProps image={logo2}
+            title="Đây là ngôn ngữ Java"
+            description="lorem idfcfsfgssfsfvsdfvsdvsdtỵuikoiujytgrfertyikjhgvcfsrtfgerfev"/>
+            <BaiTapProps image={logo3}
+            title="Đây là ngôn ngữ JS"
+            description="lorem idfcfsfgssfsfvsdfvsdvsdvfgdhyjuuiujyhtgrfdc ttrvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvwfvervet"/>
+            <BaiTapProps image={logo4}
+            title="Đây là ngôn ngữ C++"
+            description="lorem idfcfsfgssfsfvsdfvsdvsdvccdvdfgstrdyhgfdvcdfdfgfjghgfdtyulkjđẹtkhgfdbdkvncdddddddddddddddddddddddddddddscđfvdfvdvdfvvdfvdfbf"/>
+        </ul>
+        <div>
+          {/* Tối ưu code Props */}
+          <ul class="ngonNguPhuocDung">
+            <HoatDong 
+            image={myData[0].image}
+            tenHoatDong={myData[0].tenHoatDong}
+            thoiGianHoatDong={myData[0].thoiGianHoatDong}
+            ngayHoatDongTrongTuan={myData[0].ngayHoatDongTrongTuan}>
+            </HoatDong>
+            {/* Hoặc là  */}
+            <HoatDong {...myData[1]} />
+            <HoatDong {...myData[2]} />
+            <HoatDong {...myData[3]} />
+          </ul>
+        </div>
+        
       </div>
     </>
   )
 }
-export default App
-export function BaiTapProps(){
+export default App;
+
+//Props
+export function BaiTapProps(ngonNgu){
   return (
     <>
-      
+        <li>
+          <img src={ngonNgu.image}></img>
+          <h3>{ngonNgu.title}</h3>
+          <p>{ngonNgu.description}</p>
+        </li>
+    </>
+  )
+}
+//Props sử dụng destructing cho hàm
+export function HoatDong({image,tenHoatDong,thoiGianHoatDong,ngayHoatDongTrongTuan}){
+  return (
+    <>
+      <li>
+        <img src={image}></img>
+        <h2>{tenHoatDong}</h2>
+        <p>{thoiGianHoatDong}</p>
+        <p>{ngayHoatDongTrongTuan}</p>
+      </li>
     </>
   )
 }
---
+++ src/data.js
@@ -0,0 +1,29 @@
+import img from '../Image/avatar-cua-phuoc.jpg'
+
+const data=[
+    {
+        image:img,
+        tenHoatDong:"Ăn",
+        thoiGianHoatDong:"2 tiếng",
+        ngayHoatDongTrongTuan:"Hằng ngày"
+    },
+    {
+        image:img,
+        tenHoatDong:"Ngủ",
+        thoiGianHoatDong:"2 tiếng",
+        ngayHoatDongTrongTuan:"Hằng ngày"
+    },
+    {
+        image:img,
+        tenHoatDong:"Ỉa",
+        thoiGianHoatDong:"2 tiếng",
+        ngayHoatDongTrongTuan:"Hằng ngày"
+    },
+    {
+        image:img,
+        tenHoatDong:"Code",
+        thoiGianHoatDong:"18 tiếng",
+        ngayHoatDongTrongTuan:"Hằng ngày"
+    }
+]
+export default data
